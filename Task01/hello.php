144405345adb00188f4a7e9a58603e3015c2151e
artemaaaaaaaaaaaaaaaaaaa <artm-grachev@mail.ru>
2025-02-06 22:34:43 +0300

    Task01

---
+++ Task01/hello.php
@@ -0,0 +1,11 @@
+<?php
+
+date_default_timezone_set('Europe/Moscow');
+
+$dateTime = date("d.m.Y H:i:s");
+
+$FIO = 'Грачёв Артём';
+
+echo "Текущая дата и время: $dateTime";
+
+echo "\nФамилия и имя: $FIO\n";
